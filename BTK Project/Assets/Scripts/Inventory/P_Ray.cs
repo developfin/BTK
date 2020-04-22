43a5ddd028463ea928146193c7bfeabbffaac1f9
Pocik <nikitaagafonov228@mail.ru>
2020-04-22 17:51:39 +0300

    Crosshair

--- BTK Project/Assets/Scripts/Inventory/P_Ray.cs
+++ BTK Project/Assets/Scripts/Inventory/P_Ray.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class P_Ray : MonoBehaviour
 {
     public GameObject InventoryObj, Player, GrayPanel;
-    private bool pause_on, inv_open;
+    private bool pause_on, inv_open, act;
+    public Texture2D DefaultCrosshair, ActCrosshair;
     
     void Start()
     {
@@ -20,8 +21,10 @@ public class P_Ray : MonoBehaviour
             inv_open = !inv_open;
             OpenInventory();
         }
-
-        
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            act = !act;
+        }
     }
 
     void OpenInventory()
@@ -57,6 +60,15 @@ public class P_Ray : MonoBehaviour
             Cursor.lockState = CursorLockMode.Locked;
             Player.GetComponent<Player_Controller>().enabled = true;
             GrayPanel.SetActive(false);
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width / 2 - 5, Screen.height / 2 - 5, 10, 10), DefaultCrosshair);
+        if (act)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 8, Screen.height / 2 - 8, 16, 16), ActCrosshair);
         }
     }
 }
