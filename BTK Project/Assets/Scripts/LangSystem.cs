8b133fe215e5898cc8bdc775056cfd13317e0b46
Pocik <nikitaagafonov228@mail.ru>
2020-05-11 17:04:56 +0300

    Not working language settings...

---
+++ BTK Project/Assets/Scripts/LangSystem.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class LangSystem : MonoBehaviour
+{
+    public LanguageLoading TranslateTextScript;
+    public GameObject LangPanel;
+    public GameObject MenuPanel;
+    private string json;
+    public static lang lng = new lang();
+    private int langIndex;
+    private string[] langArray = {"ru_RU", "en_US"};
+
+    void Awake()
+    {
+        if (!PlayerPrefs.HasKey("Language"))
+        {
+            if(Application.systemLanguage == SystemLanguage.Russian || Application.systemLanguage == SystemLanguage.Ukrainian || Application.systemLanguage == SystemLanguage.Belarusian)
+            {
+                PlayerPrefs.SetString("Language", "ru_RU");
+            }
+            else
+            {
+                PlayerPrefs.SetString("Language", "en_US");
+            }
+        }
+        else
+        {
+            LangPanel.SetActive(false);
+            MenuPanel.SetActive(true);
+        }
+        LangLoad();
+    }
+    void LangLoad()
+    {
+        json = File.ReadAllText(Application.streamingAssetsPath + "/Languages/" + PlayerPrefs.GetString("Language") + ".json");
+        lng = JsonUtility.FromJson<lang>(json);
+    }
+
+    public void RU_Lang()
+    {
+        langIndex = 0;
+        PlayerPrefs.SetString("Language", langArray[langIndex]);
+        LangLoad();
+    }
+
+    public void ENG_Lang()
+    {
+        langIndex = 1;
+        PlayerPrefs.SetString("Language", langArray[langIndex]);
+        LangLoad();
+    }
+}
+
+public class lang
+{
+    public string[] main_menu_main;
+    public string[] main_menu_language;
+    public string[] main_menu_settings;
+    public string[] main_menu_play;
+    public string[] settings_general;
+    public string[] settings_graphic;
+    public string[] settings_audio;
+}
---
+++ BTK Project/Assets/Scripts/LanguageLoading.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanguageLoading : MonoBehaviour
+{
+    public Text[] Menu_Main_Text;
+    public Text[] Menu_Lang_Text;
+    public Text[] Menu_Settings_Text;
+    public Text[] Menu_Play_Text;
+    public Text[] Settings_General_Text;
+    public Text[] Settings_Graphic_Text;
+    public Text[] Settings_Audio_Text;
+
+    public void UpdateText()
+    {
+        for (int i = 0; i < Menu_Main_Text.Length; i++) Menu_Main_Text[i].text = LangSystem.lng.main_menu_main[i];
+        for (int a = 0; a < Menu_Lang_Text.Length; a++) Menu_Lang_Text[a].text = LangSystem.lng.main_menu_language[a];
+        for (int b = 0; b < Menu_Settings_Text.Length; b++) Menu_Settings_Text[b].text = LangSystem.lng.main_menu_settings[b];
+        for (int c = 0; c < Menu_Play_Text.Length; c++) Menu_Play_Text[c].text = LangSystem.lng.main_menu_play[c];
+        for (int d = 0; d < Settings_General_Text.Length; d++) Settings_General_Text[d].text = LangSystem.lng.settings_general[d];
+        for (int e = 0; e < Settings_Graphic_Text.Length; e++) Settings_Graphic_Text[e].text = LangSystem.lng.settings_graphic[e];
+        for (int f = 0; f < Settings_Audio_Text.Length; f++) Settings_Audio_Text[f].text = LangSystem.lng.settings_audio[f];
+    }
+}
